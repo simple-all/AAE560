762dd31058a43e9831e570505e121a1b95fbcb02
djmuller83 <dustin.j.muller@gmail.com>
2017-04-06 14:41:54 -0400

    more rubust get matrix script
    more rubust get matrix script

--- getmatrix.m
+++ getmatrix.m
@@ -1,6 +1,7 @@
-NodeInfo=zeros(2873,4);
+a=length(connectivity_matrix);
+NodeInfo=zeros(a,4);
 [bounds, node, way, ~] = assign_from_parsed(parsed_osm);
-for i=1:1:2873
+for i=1:1:a
    NodeInfo(i,1)=i;
    NodeInfo(i,4)=node.id(i);
    for j=1:1:2
